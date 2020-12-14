2297157cac42d3c286ae7993f40bce8e7c4aa616
SirdsH <svatopluk.houserek@educanet.cz>
2020-12-14 12:37:24 +0100

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,20 +11,31 @@ public class Main {
         List<Integer> arr1 = new ArrayList<>();
         Random r1 = new Random();
         for (int i = 0; i < 100; i++) {
-            arr1.add(r1.nextInt(50));
+            arr1.add(r1.nextInt(100));
+            Collections.sort(arr1);
         }
+        System.out.println(arr1);
         List<Integer> arr2 = new ArrayList<>();
         Random r2 = new Random();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             arr2.add(r2.nextInt(100));
+            Collections.sort(arr2);
         }
-        if (arr1.equals(arr2.size())){
-            System.out.println("Ano");
-        }
-        else {
-            System.out.println("Ne");
-            arr1.retainAll(arr2);
-            System.out.println(arr1);   
-        }
+        System.out.println(arr2);
+            if (arr1.equals(arr2)){
+                System.out.println("Pole jsou stejná");
+            }
+            else {
+                for (int i = 0; i < arr2.size(); i++) {
+                    if (arr1.get(i)!=arr2.get(i)){
+                        arr2.set(i, arr1.get(i));
+                    }
+                }
+                for (int i = arr1.size() -1; i > 49; i--) {
+                   arr1.remove(i);
+                }
+                System.out.println(arr1);
+                System.out.println(arr2);
+            }
     }
 }
